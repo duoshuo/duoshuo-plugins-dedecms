248f7493bd9722664f776c6957fa42936a969e23
Allen Ding <happydingchen@gmail.com>
2012-07-15 14:19:33 +0800

    反向同步测试完成

--- source/utf8/dede/duoshuo.php
+++ source/utf8/dede/duoshuo.php
@@ -7,15 +7,66 @@
  * @copyright      Copyright (c) 2012 - , Duoshuo, Inc.
  * @link           http://www.duoshuo.com
  */
+
 require_once(dirname(__FILE__)."/config.php");
 require_once(DEDEINC."/json.class.php");
-require_once('./duoshuo.class.php');
+require_once(DEDEROOT.'/plus/duoshuo/duoshuo.php');
 
-$duoshuo = new Duoshuo;
+require_once(DEDEROOT.'/plus/duoshuo/duoshuo_admin.php');
+
+// 设置默认参数
+Duoshuo::checkDefaultSettings(dirname(__FILE__));
+
+//从服务器返回的注册结果
+if(!empty($_GET) && isset($_GET['short_name']) && isset($_GET['secret'])){
+	Duoshuo::saveConfig('short_name',$_GET['short_name']);
+	Duoshuo::saveConfig('secret',$_GET['secret']);	
+}
+Duoshuo::init();
+$duoshuoAdmin = new DuoshuoAdmin();
+
+//兼容0.1.x版本插件 引入多说配置文件 {{ 只用于 0.2.x
+$configFile =  DEDEDATA.'/duoshuo.inc.php';
+
+if(file_exists($configFile)){
+	if(empty(Duoshuo::$shortName) || empty(Duoshuo::$secret)){//如果数据库设置不完整,从文件导入
+		include_once $configFile;
+		global $cfg_duoshuo;
+		if(isset($cfg_duoshuo))
+		{
+			if(empty(Duoshuo::$shortName) && !empty($cfg_duoshuo['short_name'])){
+				Duoshuo::saveConfig('short_name',$cfg_duoshuo['short_name']);
+				Duoshuo::$shortName = $cfg_duoshuo['short_name'];
+			}
+			if(empty(Duoshuo::$secret) && !empty($cfg_duoshuo['secret'])){
+				Duoshuo::saveConfig('secret',$cfg_duoshuo['secret']);
+				Duoshuo::$secret = $cfg_duoshuo['secret'];
+			}
+		}
+	}
+
+	if(!empty(Duoshuo::$shortName) && !empty(Duoshuo::$secret)){
+		$success = unlink($configFile);//如果数据库已经设置，删除文件
+	}
+}
+//}}
+
+if(empty(Duoshuo::$shortName) || empty(Duoshuo::$secret)){
+	$params = Duoshuo::packageOptions();
+	var_dump('abc');
+	$url = 'http://' . Duoshuo::DOMAIN . '/connect-site/?'. http_build_query($params, null, '&');
+	header("Location:" . $url, true);
+	exit;
+}
+
+
+if(Duoshuo::$adminPath !== dirname(__FILE__) && Duoshuo::$syncToLocal && Duoshuo::$seoEnabled){
+	//管理路径不对，发出提醒
+}
 
 if(isset($action)){
-	if (method_exists ( $duoshuo, $action ) === TRUE){
-		$duoshuo->$action();
+	if (method_exists( $duoshuoAdmin, $action ) === TRUE){
+		$duoshuoAdmin->$action();
 	}
 	else
 		Showmsg('没有此操作',1,2);
--- source/utf8/include/taglib/duoshuo.lib.php
+++ source/utf8/include/taglib/duoshuo.lib.php
@@ -1,39 +1,28 @@
-<?php   if(!defined('DEDEINC')) exit('Request Error!');
-
- 
+<?php if(!defined('DEDEINC')) exit('Request Error!');
+require_once(DEDEROOT.'/plus/duoshuo/duoshuo.php');
 function lib_duoshuo(&$ctag,&$refObj)
 {
-    global $dsql,$envs,$cfg_phpurl,$cfg_basehost,$cfg_multi_site;
-
-    $attlist='type|0';
-    FillAttsDefault($ctag->CAttribute->Items,$attlist);
-    extract($ctag->CAttribute->Items, EXTR_SKIP);
-    include(DEDEDATA.'/duoshuo.inc.php');//定义了$cfg_duoshuo
-    
-    if (!isset($cfg_duoshuo['short_name']) || !isset($cfg_duoshuo['secret']))  return '在管理后台进行一步配置，就可以开始使用多说了';
-    $short_name = $cfg_duoshuo['short_name'];
-    
-    $arcid = !empty($refObj->Fields['aid']) ? $refObj->Fields['aid'] : 0;
-    $arctitle = !empty($refObj->Fields['title']) ? $refObj->Fields['title'] : 0;
-    
-    $data_local_thread_id = !empty($arcid) ? ' data-local-thread-id="'.$arcid.'"' : '';
-    $data_title = !empty($arctitle) ? ' data-title="'.$arctitle.'"' : '';
-    
-    
-    $reval = <<<EOT
-<!-- Duoshuo Comment BEGIN -->
-	<div class="ds-thread"$data_local_thread_id$data_title></div>
-	<script type="text/javascript">
-	var duoshuoQuery = {short_name: "$short_name"};
-	(function() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = 'http://static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
-	})();
-	</script>
-<!-- Duoshuo Comment END -->
-EOT;
-    return $reval;
+	if(!Duoshuo::$initialized){
+		
+		Duoshuo::init();
+	}
+	global $dsql,$envs,$cfg_phpurl,$cfg_basehost,$cfg_multi_site;
+	$attlist='type|0';
+	FillAttsDefault($ctag->CAttribute->Items,$attlist);
+	extract($ctag->CAttribute->Items, EXTR_SKIP);
+	
+	if (empty(Duoshuo::$shortName) || empty(Duoshuo::$secret))  return '在管理后台进行一步配置，就可以开始使用多说了';
+	$short_name = Duoshuo::$shortName;
+	
+	$arcid = !empty($refObj->Fields['aid']) ? $refObj->Fields['aid'] : 0;
+	$arctitle = !empty($refObj->Fields['title']) ? $refObj->Fields['title'] : 0;
+	
+	$data_source_thread_id = !empty($arcid) ? ' data-source-thread-id="'.$arcid.'"' : '';
+	$data_title = !empty($arctitle) ? ' data-title="'.htmlspecialchars($arctitle).'"' : '';
+	
+	ob_start();
+	require (DEDEROOT.'/plus/duoshuo/templets/duoshuo_comments.htm');
+	$reval = ob_get_clean();
+	
+	return $reval;
 }
